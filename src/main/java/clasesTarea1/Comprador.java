525213f73d4a9953dcfe96716932f8770e62fa86
Jocabed Aileen <joclopez2021@inf.udec.cl>
2024-11-08 23:02:48 -0300

    se agregan posiciones específicas para addMoneda, getVuelto y getBebida

--- src/main/java/clasesTarea1/Comprador.java
+++ src/main/java/clasesTarea1/Comprador.java
@@ -23,18 +23,40 @@ public class Comprador {
     }
 
     public void addMoneda100(){
-
+        Moneda m=new Moneda100(serieMoneda++,0,0);
+        m.setXY(x+Mon100+20,y+320);
+        System.out.println("Serie: "+m.getSerie());
+        monedero.add(m);
+        Mon100-=2;
+        dinero+=m.getValor();
     }
 
     public void addMoneda500(){
-
+        Moneda m=new Moneda500(serieMoneda++,0,0);
+        m.setXY(x+Mon500+35*4, y+320);
+        System.out.println("Serie: "+m.getSerie());
+        monedero.add(m);
+        Mon500-=2;
+        dinero+=m.getValor();
     }
     public void addMoneda1000(){
-
+        Moneda m=new Moneda1000(serieMoneda++,0,0);
+        m.setXY(x+Mon1000+(30*4)*2+30, y+320);
+        System.out.println("Serie: "+m.getSerie());
+        monedero.add(m);
+        Mon1000-=2;
+        dinero+=m.getValor();
     }
 
     public Moneda getVuelto(){
-
+        Moneda m=exp.getVuelto();
+        if(m!=null){
+            dinero+=m.getValor();
+            System.out.println("Vuelto: "+m.getValor());
+            m.setXY(x+10+vuelto, y+417);
+            vuelto+=2;
+            monedero.add(m);
+        }return m;
     }
 
     public void resetMonedas(){
@@ -45,13 +67,21 @@ public class Comprador {
     }
 
     public void cualBebida(int cual) throws PagoIncorrectoException, PagoInsuficienteException, NoHayBebidaException{
-
+        resetMonedas();
+        dinero=0;
+        exp.comprarBebida(monedero, cual);
+        monedero.clear();
     }
 
     public void getBebida(){
-
+        Bebida b=exp.getDepUnico();
+        if(b!=null){
+           auxB+=5;
+           System.out.println("Bebida: "+b.beber()+ "Serie: "+b.getSerie());
+           b.setXY(x+10, y+500+auxB);
+           beb.add(b);
+        }
     }
-
 
     public int cuantoVuelto(){
         return vuelto;
